7435a77d347e5cc07beb23dca5023f37660cd22a
Noa Houssier <nestornebula.dev@gmail.com>
2025-11-12 20:17:37 +0100

    fix: remove tests that expected wrong values

--- tests/src/ft_calloc.c
+++ tests/src/ft_calloc.c
@@ -22,27 +22,3 @@ Test(ft_calloc, initializes_every_element_to_zero)
 			"ft_calloc: expected all members to be set to zero");
 	free(p);
 }
-
-Test(ft_calloc, returns_null_pointer_on_empty_nmemb_size)
-{
-	void	*p;
-
-	p = ft_calloc(0, SIZE);
-	cr_expect(zero(ptr, p),
-		"ft_calloc: expected to return a null pointer on empty nmemb");
-	free(p);
-	p = ft_calloc(NMEMB, 0);
-	cr_expect(zero(ptr, p),
-		"ft_calloc: expected to return a null pointer on empty size");
-	free(p);
-}
-
-Test(ft_calloc, returns_null_pointer_that_can_be_freed_on_size_overflow)
-{
-	void	*p;
-
-	p = ft_calloc(NMEMB, INT_MAX);
-	cr_expect(zero(ptr, p),
-		"ft_calloc: expected to return a null pointer on size overflow");
-	free(p);
-}
