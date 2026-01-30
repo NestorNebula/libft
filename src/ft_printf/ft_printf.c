4c225401050761e47a6f9d8e8ee2aed771c8e5ed
Noa Houssier <nhoussie@student.42.fr>
2026-01-30 10:38:18 +0100

    feat: remove err variable from ft_printf

--- src/ft_printf/ft_printf.c
+++ src/ft_printf/ft_printf.c
@@ -15,12 +15,12 @@
 #include <unistd.h>
 #include "conv.h"
 
-static void	handle_char(char c, t_string *str, bool *err);
+static void	handle_char(char c, t_string *str);
 
 static void	handle_conversion(const char **format, t_string *str,
-				va_list *ap, bool *err);
+				va_list *ap);
 
-static int	handle_output(t_string *str, bool err);
+static int	handle_output(t_string *str);
 
 /**
  *  Writes formatted output to stdout using format and others arguments.
@@ -32,12 +32,10 @@ static int	handle_output(t_string *str, bool err);
 int	ft_printf(const char *format, ...)
 {
 	int			res;
-	bool		err;
 	va_list		ap;
 	t_string	*str;
 
 	res = 0;
-	err = false;
 	if (format == NULL)
 		return (-1);
 	str = new_string();
@@ -47,22 +45,21 @@ int	ft_printf(const char *format, ...)
 	while (*format != '\0')
 	{
 		if (*format != '%')
-			handle_char(*format++, str, &err);
+			handle_char(*format++, str);
 		else
-			handle_conversion(&format, str, &ap, &err);
+			handle_conversion(&format, str, &ap);
 	}
 	va_end(ap);
-	return (handle_output(str, err));
+	return (handle_output(str));
 }
 
-static void	handle_char(char c, t_string *str, bool *err)
+static void	handle_char(char c, t_string *str)
 {
-	if (string_cat(str, &c, 1) == NULL)
-		*err = true;
+	string_cat(str, &c, 1);
 }
 
 static void	handle_conversion(const char **format, t_string *str,
-				va_list *ap, bool *err)
+				va_list *ap)
 {
 	t_conv	conv;
 
@@ -72,11 +69,9 @@ static void	handle_conversion(const char **format, t_string *str,
 	if (conv.err)
 	{
 		if (conv.type != '\0')
-			handle_char('%', str, err);
+			handle_char('%', str);
 	}
-	else if (set_conv_val(ap, &conv) == NULL)
-		*err = true;
-	else
+	else if (set_conv_val(ap, &conv) != NULL)
 	{
 		set_conv_pref(&conv);
 		print_conv(&conv, str);
@@ -84,17 +79,11 @@ static void	handle_conversion(const char **format, t_string *str,
 	}
 }
 
-static int	handle_output(t_string *str, bool err)
+static int	handle_output(t_string *str)
 {
 	int	res;
 
-	if (err || str->content == NULL)
-	{
-		free_string(str);
-		return (-1);
-	}
 	res = write(STDOUT_FILENO, str->content, str->len);
 	free_string(str);
 	return (res);
-
 }
